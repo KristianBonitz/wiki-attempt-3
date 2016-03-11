531592c59feb1e1ac29c1a26cf05d7125cdb13a5
Kristian Bonitz <kobonitz@gmail.com>
2016-03-11 12:36:38 +1100

    Syntax corrections, jQuery edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function($){
-	$($.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='
-		+ search + '&srlimit=100', function(json) {
-			/*optional stuff to do after success */
-	});)
+	$(".search").click(function() {
+		var search-query = $(".search-form").text();
+		if(search-query !== ""){
+
+			$($.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='
+			+ search-query + '&srlimit=100', function(json) {
+				var html = "";
+
+				for (var i = json.query.search .length - 1; i >= 0; i--) {
+					json.query.search[i]
+				};
+			});)
+		}
+	});
+
+	
 });
