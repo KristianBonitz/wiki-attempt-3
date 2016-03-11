8de846623ee1faac1f085f2175e43cda116427de
Kristian Bonitz <kobonitz@gmail.com>
2016-03-11 12:41:51 +1100

    Syntax fix.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function($){
 	$(".search").click(function() {
-		var search-query = $(".search-form").text();
-		if(search-query !== ""){
+		var query = $(".searchForm").text();
+		if(query !== ""){
 
 			$($.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='
-			+ search-query + '&srlimit=100', function(json) {
+			+ query + '&srlimit=100', function(json) {
 				var html = "";
 
 				for (var i = json.query.search .length - 1; i >= 0; i--) {
-					json.query.search[i]
+					html += json.query.search[i].title;
+					html += "\n" + json.query.search[i].snippet
+					html += "\n\n";
 				};
+
+				$(".debug").html(html);
 			});)
 		}
 	});
-
-	
 });
