1c7a1aa363c7d2a655b73b79cf64969251e197fd
KristianBonitz <kobonitz@gmail.com>
2016-03-16 10:40:19 +1100

    Fixed XML Error.
    Replaced the .getJSON with .ajax which makes it much cleaner as well as
    stops the XML error from occurring. Major Fix.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,47 @@ $(document).ready(function($){
 		var query = $(".searchForm").val();
 		if(query !== ""){
 
-			$.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='
-			+ query + '&srlimit=100', function(json) {
-				var html = "";
+			$.ajax({
+				url: 'https://en.wikipedia.org/w/api.php?',
+				type: 'GET',
+				jsonp: 'callback',
+				dataType: 'jsonp',
+				data: {
+					action: 'query',
+					format: 'json',
+					list: 'search',
+					srsearch: query,
+					srlimit: 100
+				},
+				xhrFields: {
+					withCredentials: true
+				}
 
+			})
+			.done(function(json) {
+				console.log("success");
+
+				var html = "";
 				for (var i = json.query.search .length - 1; i >= 0; i--) {
 					html += json.query.search[i].title;
-					html += "\n" + json.query.search[i].snippet
-					html += "\n\n";
+					html += "<br>" + json.query.search[i].snippet
+					html += "<br><br>";
 				};
 
 				$(".debug").html(html);
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
 			});
+			/*
+			$.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='
+			+ query + '&srlimit=100', function(json) {
+				
+			});
+			*/
 		}
 	});
 });
